3db6cb653adc354f9b694421b028b63fe2482976
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-08-17 10:31:13 +0800

    reduce memory consumption

--- ml/utility/gradient_checking.cpp
+++ ml/utility/gradient_checking.cpp
@@ -20,10 +20,8 @@ void gradient_checking::initialize(const cv::Mat &theta)
     CV_Assert(theta.channels() == 1 &&
               theta.rows == 1);
 
-    theta.copyTo(theta_minus_);
-    theta.copyTo(theta_plus_);
-    theta_minus_.convertTo(theta_minus_, CV_64F);
-    theta_plus_.convertTo(theta_plus_, CV_64F);
+    theta.copyTo(theta_buffer_);
+    theta_buffer_.convertTo(theta_buffer_, CV_64F);
 }
 
 }}
--- ml/utility/gradient_checking.hpp
+++ ml/utility/gradient_checking.hpp
@@ -45,8 +45,7 @@ private:
 
     cv::Mat gradient_;
 
-    cv::Mat theta_minus_;
-    cv::Mat theta_plus_;
+    cv::Mat theta_buffer_;
 };
 
 /**
@@ -66,21 +65,19 @@ compute_gradient(const cv::Mat &theta,
                  UnaryFunc cost_function)
 {
     initialize(theta);
+    double *buffer_ptr = theta_buffer_.ptr<double>(0);
     double *gradient_ptr = gradient_.ptr<double>(0);
-    double *minus_ptr = theta_minus_.ptr<double>(0);
-    double *plus_ptr = theta_plus_.ptr<double>(0);
     double *theta_ptr = theta.ptr<double>(0);
     for(int i = 0; i != theta.cols; ++i){
-        *minus_ptr = *theta_ptr - epsillon_;
-        *plus_ptr = *theta_ptr + epsillon_;
-        *gradient_ptr = (cost_function(theta_plus_) - cost_function(theta_minus_)) /
-                2 * epsillon_;
-        *minus_ptr = *theta_ptr;
-        *plus_ptr = *theta_ptr;
+        *buffer_ptr = *theta_ptr - epsillon_;
+        double const Plus = cost_function(theta_buffer_);
+        *buffer_ptr = *theta_ptr - epsillon_;
+        double const Minus = cost_function(theta_buffer_);
+        *gradient_ptr = (Plus - Minus) / (2 * epsillon_);
 
+        *buffer_ptr = *theta_ptr;
+        ++buffer_ptr;
         ++gradient_ptr;
-        ++minus_ptr;
-        ++plus_ptr;
         ++theta_ptr;
     }
 
