0239c8d2ceb0a8b2e2c95a27841bc2e428dda4fb
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-08-25 19:14:32 +0800

    add function to convert cv mat to row major eigen matrix

--- eigen/eigen.hpp
+++ eigen/eigen.hpp
@@ -1,6 +1,8 @@
 #ifndef CV_TO_EIGEN_HPP
 #define CV_TO_EIGEN_HPP
 
+#include <opencv2/core.hpp>
+
 #include <Eigen/Dense>
 
 /*! \file cv_to_eigen.hpp
@@ -24,6 +26,15 @@ template<typename T>
 using MatRowMajor =
 Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic,
 Eigen::RowMajor>;
+
+template<typename T>
+void cv2eigen_cpy(cv::Mat const &input, MatRowMajor<T> &output){
+    output.resize(input.rows, input.cols);
+    cv::Mat dst(input.rows, input.cols, cv::DataType<T>::type,
+                output.data(),
+                (size_t)(output.stride()*sizeof(double)));
+    input.convertTo(dst, dst.type());
+}
 
 } /*! @} End of Doxygen Groups*/
 
