3f73569596a26e07a6248e7cfb40bb8e29d5f379
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-08-27 01:27:35 +0800

    refine gradient checking; support Eigen matrix

--- ml/utility/gradient_checking.hpp
+++ ml/utility/gradient_checking.hpp
@@ -31,24 +31,45 @@ class gradient_checking
 public:
     gradient_checking();
 
-    bool compare_gradient(cv::Mat const &gradient) const;
+    //bool compare_gradient(cv::Mat const &gradient) const;
+
+    template<typename EigenMat>
+    bool compare_gradient(EigenMat const &grad_1,
+                          EigenMat const &grad_2) const;
 
     template<typename T, typename UnaryFunc>
-    cv::Mat const& compute_gradient(cv::Mat const &theta,
-                                    UnaryFunc func);
+    cv::Mat compute_gradient(cv::Mat const &theta,
+                             UnaryFunc cost_function) const;
+
+    template<typename EigenMat, typename UnaryFunc>
+    EigenMat compute_gradient(EigenMat const &theta,
+                              UnaryFunc cost_function) const;
 
     void set_epsillon(double epsillon);
     void set_inaccuracy(double inaccuracy);
 
 private:
     double epsillon_;
-
-    cv::Mat gradient_;
-
     double inaccuracy_;
-
-    cv::Mat theta_buffer_;
 };
+
+/**
+ * @brief compute two gradient, make sure their different are small
+ * @param grad_1 the gradient want to compute with grad_2
+ * @param grad_2 the gradient want to compute with grad_1
+ * @return true if the inaccuracy is low enough, else false
+ */
+template<typename EigenMat>
+bool gradient_checking::
+compare_gradient(EigenMat const &grad_1,
+                 EigenMat const &grad_2) const
+{
+    static_assert(!std::is_same<cv::Mat, EigenMat>::value,
+                  "This function do not support opencv yet");
+    EigenMat const Diff =
+            (grad_1.array() - grad_2.array()).abs();
+    return (Diff.array() < inaccuracy_ ).all();
+}
 
 /**
  *@brief compute the gradient by gradient checking.
@@ -62,31 +83,71 @@ private:
  *@return the gradients
  */
 template<typename T, typename UnaryFunc>
-cv::Mat const &gradient_checking::
+cv::Mat gradient_checking::
 compute_gradient(const cv::Mat &theta,
-                 UnaryFunc cost_function)
+                 UnaryFunc cost_function) const
 {    
-    theta.copyTo(theta_buffer_);
-    gradient_.create(theta.rows,
-                     theta.cols,
-                     theta.type());
+    cv::Mat theta_buffer;
+    theta.copyTo(theta_buffer);
+    cv::Mat gradient;
+    gradient.create(theta.rows,
+                    theta.cols,
+                    theta.type());
     for(int row = 0; row != theta.rows; ++row){
         for(int col = 0; col != theta.cols; ++col){
-            auto const OriValue = theta_buffer_.at<T>(row, col);
-            theta_buffer_.at<T>(row, col) =
+            auto const OriValue = theta_buffer.at<T>(row, col);
+            theta_buffer.at<T>(row, col) =
                     OriValue + epsillon_;
-            auto const Plus = cost_function(theta_buffer_);
-            theta_buffer_.at<T>(row, col) =
+            auto const Plus = cost_function(theta_buffer);
+            theta_buffer.at<T>(row, col) =
                     OriValue - epsillon_;
-            auto const Minus = cost_function(theta_buffer_);
-            gradient_.at<T>(row, col) =
+            auto const Minus = cost_function(theta_buffer);
+            gradient.at<T>(row, col) =
                     (Plus - Minus) / (2 * epsillon_);
 
-            theta_buffer_.at<T>(row, col) = OriValue;
+            theta_buffer.at<T>(row, col) = OriValue;
         }
     }
 
-    return gradient_;
+    return gradient;
+}
+
+/**
+ *@brief compute the gradient by gradient checking.
+ *@param theta The "weights" which need to optimize by\n
+ * the gradient descent like algo
+ *@param cost_function The cost function of the algo, this\n
+ * function only accept one parameter--cv::Mat which contains\n
+ * the (theta[i] + epsillon) or (theta[i] - epsillon)
+ *@pre <strong class = "paramname">theta</strong> must be\n
+ * one channel
+ *@return the gradients
+ */
+template<typename EigenMat, typename UnaryFunc>
+EigenMat gradient_checking::
+compute_gradient(EigenMat const &theta,
+                 UnaryFunc cost_function) const
+{
+    EigenMat theta_buffer = theta;
+    EigenMat gradient = EigenMat::Zero(theta_buffer.rows(),
+                                       theta_buffer.cols());
+    for(int row = 0; row != theta.rows(); ++row){
+        for(int col = 0; col != theta.cols(); ++col){
+            auto const OriValue = theta_buffer(row, col);
+            theta_buffer(row, col) =
+                    OriValue + epsillon_;
+            auto const Plus = cost_function(theta_buffer);
+            theta_buffer(row, col) =
+                    OriValue - epsillon_;
+            auto const Minus = cost_function(theta_buffer);
+            gradient(row, col) =
+                    (Plus - Minus) / (2 * epsillon_);
+
+            theta_buffer(row, col) = OriValue;
+        }
+    }
+
+    return gradient;
 }
 
 } /*! @} End of Doxygen Groups*/
