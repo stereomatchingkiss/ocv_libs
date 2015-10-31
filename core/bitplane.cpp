ec12019b66119fe5284177e6509b05249b048527
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-11-01 06:56:39 +0800

    support single thread version if do not support intel tbb

--- core/bitplane.cpp
+++ core/bitplane.cpp
@@ -11,11 +11,12 @@ void bitplane_generator(const cv::Mat &input, std::vector<cv::Mat> &output)
     CV_Assert(input.type() == CV_8U);
 
     output.resize(8);    
-    uchar const mask[] = {1, 2, 4, 8, 16, 32, 64, 128};
+    uchar const mask[] = {1, 2, 4, 8, 16, 32, 64, 128};    
 
+#ifdef OCV_SUPPORT_INTEL_TBB
     tbb::parallel_for(tbb::blocked_range<size_t>(0, 8),
                       [&](tbb::blocked_range<size_t> const &r)
-    {
+    {        
         for(size_t i = r.begin(); i != r.end(); ++i){
             output[i].create(input.rows, input.cols, input.type());
             ocv::for_each_channels<uchar>(input, output[i],
@@ -25,15 +26,16 @@ void bitplane_generator(const cv::Mat &input, std::vector<cv::Mat> &output)
             });
         }
     });
-
-    /*for(size_t i = 0; i != output.size(); ++i){
-        ocv::for_each<uchar>(input, output[i],
-                             [=](uchar a, uchar &b)
-        {
-            std::bitset<sizeof(uchar)> bits(a);
-            b = bits[i];
+#else
+    for(size_t i = 0; i != output.size(); ++i){
+        output[i].create(input.rows, input.cols, input.type());
+        ocv::for_each_channels<uchar>(input, output[i],
+                             [&](uchar a, uchar &b)
+        {            
+            b = (a & mask[i]) == mask[i] ? 1 : 0;
         });
     }//*/
+#endif
 }
 
 }
