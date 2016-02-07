62ddc08f020e8a34a1887ae83d779c9535b4e94b
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-02-07 22:25:35 +0800

    add overload, easier to reuse buffer

--- tiny_cnn/image_converter.hpp
+++ tiny_cnn/image_converter.hpp
@@ -21,22 +21,22 @@ namespace ocv{
 namespace tiny_cnn{
 
 template<typename Img>
-Img cvmat_to_img(cv::Mat const& input,
-                 float_t scale_min = -1,
-                 float_t scale_max = 1)
+void cvmat_to_img(cv::Mat const& input,
+                  Img &output,
+                  float_t scale_min = -1,
+                  float_t scale_max = 1)
 {
     CV_Assert(input.type() == CV_8U || input.type() == CV_8UC3);
     CV_Assert(scale_min < scale_max);
 
-    Img img;
     if(input.type() == CV_8UC3){
         int const area = input.rows * input.cols;
-        img.resize(area*3);
+        output.resize(area*3);
         for (int i = 0; i < input.rows; i++) { // Go over all rows
             int const width = input.cols*i;
             for (int j = 0; j < input.cols; j++) { // Go over all columns
                 for (int c = 0; c < 3; c++) { // Go through all channels
-                    img[area*c + width + j] =
+                    output[area*c + width + j] =
                             input.at<cv::Vec3b>(i, j)[c]/255.0*
                             (scale_max - scale_min) + scale_min;
                 }
@@ -44,13 +44,23 @@ Img cvmat_to_img(cv::Mat const& input,
         }
     }else{
         int const area = input.rows * input.cols;
-        img.resize(area);
+        output.resize(area);
         size_t index = 0;
         ocv::for_each_channels<uchar>(input, [&](uchar c)
         {
-            img[index++] = scale_min + (scale_max - scale_min) * c / 255.0;
+            output[index++] = scale_min + (scale_max - scale_min) * c / 255.0;
         });
     }
+}
+
+template<typename Img>
+inline
+Img cvmat_to_img(cv::Mat const& input,
+                 float_t scale_min = -1,
+                 float_t scale_max = 1)
+{
+    Img img;
+    cvmat_to_img(input, img, scale_min, scale_max);
 
     return img;
 }
