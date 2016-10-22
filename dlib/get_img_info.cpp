4c3968ad2ee984faf20079dca825470acc4df595
stereomatchingkiss <thamngapwei@gmail.com>
2016-10-22 21:16:00 +0800

    first commit

---
+++ dlib/get_img_info.cpp
@@ -0,0 +1,105 @@
+#include "get_img_info.hpp"
+
+#include <dlib/xml_parser.h>
+
+namespace ocv{
+
+
+namespace odlib{
+
+class doc_handler : public dlib::document_handler
+{
+public:
+    doc_handler(std::vector<std::string> &img_name,
+                std::vector<std::vector<dlib::rectangle>> &location) :
+        img_name_(img_name),
+        location_(location)
+    {
+
+    }
+
+    virtual void start_document () override
+    {
+    }
+
+    virtual void end_document () override
+    {
+    }
+
+    virtual void start_element (
+            const unsigned long,
+            const std::string& name,
+            const dlib::attribute_list& atts) override
+    {
+        std::cout<<name<<std::endl;
+        if(name == "image"){
+            img_name_.emplace_back(name);
+            location_.emplace_back();
+        }else if(name == "box"){
+            atts.reset();
+            dlib::rectangle rect;
+            long h = 0;
+            enum {height, left, top, width};
+            for(size_t index = 0; atts.move_next(); ++index){
+                switch(index){
+                case height :{
+                    h = std::stol(atts.element().value());
+                    break;
+                }
+                case left :{
+                    rect.set_left(std::stol(atts.element().value()));
+                    break;
+                }
+                case top :{
+                    rect.set_top(std::stol(atts.element().value()));
+                    break;
+                }
+                case width:{
+                    rect.set_right(rect.left() + std::stol(atts.element().value()));
+                    break;
+                }
+                default:{
+                    break;
+                }
+                }
+            }
+            rect.set_bottom(rect.top() + h);
+            location_.back().emplace_back(rect);//*/
+        }
+    }
+
+    virtual void end_element (
+            const unsigned long,
+            const std::string&
+            ) override
+    {
+    }
+
+    virtual void characters (
+            const std::string&
+            ) override
+    {
+    }
+
+    virtual void processing_instruction (const unsigned long, const std::string&,
+                                         const std::string&
+                                         ) override
+    {
+    }
+
+private:
+    std::vector<std::string> &img_name_;
+    std::vector<std::vector<dlib::rectangle>> &location_;
+};
+
+void get_image_info(std::vector<std::string> &img_name,
+                    std::vector<std::vector<dlib::rectangle>> &location,
+                    const std::string &file_name)
+{
+    doc_handler handler(img_name, location);
+    dlib::parse_xml(file_name, handler);
+}
+
+}
+
+}
---
+++ dlib/get_img_info.hpp
@@ -0,0 +1,31 @@
+#ifndef OCV_DLIB_GET_IMG_INFO_HPP
+#define OCV_DLIB_GET_IMG_INFO_HPP
+
+#include <dlib/geometry.h>
+
+#include <string>
+#include <vector>
+
+/*!
+ *  \addtogroup ocv
+ *  @{
+ */
+namespace ocv{
+
+/*!
+ *  \addtogroup odlib
+ *  @{
+ */
+namespace odlib{
+
+
+void get_image_info(std::vector<std::string> &img_name,
+                    std::vector<std::vector<dlib::rectangle>> &location,
+                    std::string const &file_name);
+
+
+} /*! @} End of Doxygen Groups*/
+
+} /*! @} End of Doxygen Groups*/
+
+#endif // OCV_DLIB_GET_IMG_NAME_AND_POSITION_HPP
