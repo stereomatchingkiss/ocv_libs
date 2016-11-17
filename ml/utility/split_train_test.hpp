b2dd217ddbbcf753cab50bd07b8956188be3c1d4
stereomatchingkiss <thamngapwei@gmail.com>
2016-11-17 15:03:05 +0800

    1 : add new functions--split_to_kset_balance 2 : make label as const

--- ml/utility/split_train_test.hpp
+++ ml/utility/split_train_test.hpp
@@ -24,6 +24,71 @@ namespace ocv{
 namespace ml{
 
 /**
+ * split input_data and input_label to k set data.
+ * @param input_data input data want to split
+ * @param input_label input label want to split
+ * @return std::map<label_type, std::vector<std::vector<data_type>>.
+ * Key is the label, value is the data sets of each label.
+ * @code
+ * std::vector<std::string> input_data;
+ * std::vector<std::string> input_label;
+ * std::tie(input_data, input_label) = read_data();
+ * //Assume there are 2 labels, each label contain 10000 instance.
+ * //After split, the result will contain 2 keys(cat, dog), each key
+ * //associate with 5 groups of data, every contain 2000 instance.
+ * auto result = split_to_k_set_balance(input_data, input_label, 5);
+ * @endcode
+ */
+template<typename Data, typename Label>
+auto split_to_kset_balance(Data &input_data, Label const &input_label, size_t kset)
+{
+    static_assert((std::is_copy_constructible<Data>::value &&
+                   std::is_copy_constructible<Label>::value) ||
+                  (std::is_move_constructible<Data>::value &&
+                   std::is_move_constructible<Label>::value),
+                  "type Data and type Label must be copy constructible or "
+                  "move constructible");
+
+    using label_type = std::decay<decltype(input_label[0])>::type;
+    using data_type = std::decay<decltype(input_data[0])>::type;
+
+    if(kset < 2){
+        return std::map<label_type, std::vector<std::vector<data_type>>>{};
+    }
+
+    std::map<label_type, std::vector<data_type>> category;
+    for(size_t i = 0; i != input_data.size(); ++i){
+        auto it = category.find(input_label[i]);
+        if(it != std::end(category)){
+            (it->second).emplace_back(std::move(input_data[i]));
+        }else{
+            category.insert({input_label[i], {}});
+        }
+    }
+
+    std::map<label_type, std::vector<std::vector<data_type>>> results;
+    for(auto &pair : category){
+        size_t const set_size = pair.second.size() / kset;
+        size_t const residual = pair.second.size() % kset;
+        std::vector<std::vector<data_type>> data_sets;
+        auto &pdata = pair.second;
+        for(size_t i = 0; i != kset; ++i){
+            std::vector<data_type> data;
+            size_t const cur_size = i != kset - 1 ? set_size : set_size + residual;
+            for(size_t j = 0; j != cur_size && !pdata.empty(); ++j){
+                data.emplace_back(std::move(pdata.back()));
+                pdata.pop_back();
+            }
+            data_sets.emplace_back(std::move(data));
+        }
+        results.insert({pair.first, std::move(data_sets)});
+    }
+
+    return results;
+}
+
+
+/**
  * split input data and input label to two sets of data, this function
  * may change the content of input_data and input_label if they can be
  * swapped, this way it could avoid expensive copy operation. Difference
@@ -55,10 +120,10 @@ namespace ml{
  * @endcode
  */
 template<typename Data, typename Label>
-split_train_test_inplace_balance(Data &input_data, Label &input_label,
-                                 Data &train_data, Label &train_label,
-                                 Data &test_data, Label &test_label,
-                                 double test_ratio)
+void split_train_test_inplace_balance(Data &input_data, Label const &input_label,
+                                      Data &train_data, Label &train_label,
+                                      Data &test_data, Label &test_label,
+                                      double test_ratio)
 {
     static_assert((std::is_copy_constructible<Data>::value &&
                    std::is_copy_constructible<Label>::value) ||
@@ -96,7 +161,7 @@ split_train_test_inplace_balance(Data &input_data, Label &input_label,
 
 template<typename Data, typename Label>
 std::tuple<Data, Label, Data, Label>
-split_train_test_inplace_balance(Data &input_data, Label &input_label,
+split_train_test_inplace_balance(Data &input_data, Label const &input_label,
                                  double test_ratio)
 {
     Data test_data;
