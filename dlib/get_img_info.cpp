47d9ecfa7d5eeee7d297a9c7486bf64c0f74e04b
stereomatchingkiss <thamngapwei@gmail.com>
2017-01-15 17:04:18 +0800

    add overload of  mmod_rect to function get_imglab_xml_info

--- dlib/get_img_info.cpp
+++ dlib/get_img_info.cpp
@@ -9,11 +9,12 @@ namespace ocv{
 
 namespace odlib{
 
+template<typename rect_type = dlib::mmod_rect>
 class doc_handler : public dlib::document_handler
 {
 public:
     doc_handler(std::vector<std::string> &img_name,
-                std::vector<std::vector<dlib::rectangle>> &location) :
+                std::vector<std::vector<rect_type>> &location) :
         img_name_(img_name),
         location_(location)
     {
@@ -37,39 +38,19 @@ public:
             atts.reset();
             if(atts.move_next()){
                 img_name_.emplace_back(atts.element().value());
-                location_.emplace_back();
+                location_.emplace_back(std::vector<rect_type>());
             }
         }else if(name == "box"){
             atts.reset();
-            dlib::rectangle rect;
+            rect_type rect;
+            //dlib::mmod_rect
             long h = 0;
-            enum {height, left, top, width};
+            enum {height, left, top, width, ignore};
             for(size_t index = 0; atts.move_next(); ++index){
-                switch(index){
-                case height :{
-                    h = std::stol(atts.element().value()) - 1;
-                    h = std::max<long>(0, h);
-                    break;
-                }
-                case left :{
-                    rect.set_left(std::stol(atts.element().value()));
-                    break;
-                }
-                case top :{
-                    rect.set_top(std::stol(atts.element().value()));
-                    break;
-                }
-                case width:{
-                    rect.set_right(rect.left() + std::stol(atts.element().value()) - 1);
-                    rect.set_right(std::max<long>(0, rect.right()));
-                    break;
-                }
-                default:{
-                    break;
-                }
-                }
+                std::string const &attr_name = atts.element().key();
+                process_attr(attr_name, atts, rect, h);
             }
-            rect.set_bottom(rect.top() + h);
+            process_bottom(rect, h);
             location_.back().emplace_back(rect);
         }
     }
@@ -94,15 +75,61 @@ public:
     }
 
 private:
+    void process_attr(std::string const &name,
+                      dlib::attribute_list const &atts,
+                      dlib::mmod_rect &rect, long &h) const
+    {
+        if(name == "ignore"){
+            rect.ignore = true;
+        }else{
+            process_attr(name, atts, rect.rect, h);
+        }
+    }
+
+    void process_attr(std::string const &name,
+                      dlib::attribute_list const &atts,
+                      dlib::rectangle &rect, long &h) const
+    {
+        if(name == "height"){
+            h = std::stol(atts.element().value()) - 1;
+            h = std::max<long>(0, h);
+        }else if(name == "left"){
+            rect.set_left(std::stol(atts.element().value()));
+        }else if(name == "top"){
+            rect.set_top(std::stol(atts.element().value()));
+        }else if(name == "width"){
+            rect.set_right(rect.left() + std::stol(atts.element().value()) - 1);
+            rect.set_right(std::max<long>(0, rect.right()));
+        }
+    }
+
+    void process_bottom(dlib::mmod_rect &rect, long h) const
+    {
+        process_bottom(rect.rect, h);
+    }
+
+    void process_bottom(dlib::rectangle &rect, long h) const
+    {
+        rect.set_bottom(rect.top() + h);
+    }
+
     std::vector<std::string> &img_name_;
-    std::vector<std::vector<dlib::rectangle>> &location_;
+    std::vector<std::vector<rect_type>> &location_;
 };
 
 void get_imglab_xml_info(std::vector<std::string> &img_name,
-                    std::vector<std::vector<dlib::rectangle>> &roi,
-                    const std::string &file_name)
+                         std::vector<std::vector<dlib::rectangle>> &roi,
+                         const std::string &file_name)
 {
-    doc_handler handler(img_name, roi);
+    doc_handler<dlib::rectangle> handler(img_name, roi);
+    dlib::parse_xml(file_name, handler);
+}
+
+void get_imglab_xml_info(std::vector<std::string> &img_name,
+                         std::vector<std::vector<dlib::mmod_rect>> &roi,
+                         std::string const &file_name)
+{
+    doc_handler<dlib::mmod_rect> handler(img_name, roi);
     dlib::parse_xml(file_name, handler);
 }
 
