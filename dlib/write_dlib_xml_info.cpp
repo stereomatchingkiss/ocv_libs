ac7ce5c2de9f33b06842300c3e94d15b824133db
stereomatchingkiss <thamngapwei@gmail.com>
2017-06-24 22:13:21 +0800

    forgot to commit

--- dlib/write_dlib_xml_info.cpp
+++ dlib/write_dlib_xml_info.cpp
@@ -10,19 +10,22 @@ namespace ocv{
 
 namespace odlib{
 
-void write_dlib_xml(const std::vector<std::string> &image_location, const std::vector<std::vector<dlib::rectangle>> &rects,
-                    const std::string &prepend_folder, const std::string &output_name)
+namespace{
+
+pugi::xml_node write_imglab_xml_preparation(size_t image_location_size,
+                                            size_t rect_size,
+                                            const std::string &output_name,
+                                            std::ofstream &stream, pugi::xml_document &doc)
 {
-    std::ofstream stream(output_name);
+    stream.open(output_name);
     if(!stream.is_open()){
         throw std::runtime_error("cannot open file:" + output_name);
     }
 
-    if(image_location.size() != rects.size()){
+    if(image_location_size != rect_size){
         throw std::runtime_error("image_location.size() != rects.size()");
     }
 
-    pugi::xml_document doc;
     auto declarationNode = doc.append_child(pugi::node_declaration);
     declarationNode.append_attribute("version")  = "1.0";
     declarationNode.append_attribute("encoding") = "ISO-8859-1";
@@ -33,39 +36,66 @@ void write_dlib_xml(const std::vector<std::string> &image_location, const std::v
     pugi::xml_node node = doc.append_child("dataset");
     node.append_child("name").append_child(pugi::node_pcdata).set_value("imglab dataset");
     node.append_child("comment").append_child(pugi::node_pcdata).set_value("Created by imglab tool.");
-    pugi::xml_node images = node.append_child("images");
+
+    return node.append_child("images");
+}
+
+void add_image_info(pugi::xml_node &image, std::vector<dlib::rectangle> const &rects)
+{
+    for(auto const &rect : rects){
+        pugi::xml_node box = image.append_child("box");
+        box.append_attribute("top") = rect.top();
+        box.append_attribute("left") = rect.left();
+        box.append_attribute("width") = rect.width();
+        box.append_attribute("height") = rect.height();
+    }
+}
+
+void add_image_info(pugi::xml_node &image, std::vector<dlib::mmod_rect> const &rects)
+{
+    for(auto const &mrect : rects){
+        pugi::xml_node box = image.append_child("box");
+        auto const &rect = mrect.rect;
+        box.append_attribute("top") = rect.top();
+        box.append_attribute("left") = rect.left();
+        box.append_attribute("width") = rect.width();
+        box.append_attribute("height") = rect.height();
+        if(mrect.ignore){
+            box.append_attribute("height") = "1";
+        }
+    }
+}
+
+template<typename T>
+void write_imglab_xml_impl(const std::vector<std::string> &image_location, const std::vector<std::vector<T>> &rects,
+                           const std::string &prepend_folder, const std::string &output_name)
+{
+    std::ofstream stream;
+    pugi::xml_document doc;
+    pugi::xml_node images = write_imglab_xml_preparation(image_location.size(), rects.size(), output_name, stream, doc);
 
     using namespace boost::filesystem;
-
     for(size_t i = 0; i != image_location.size(); ++i){
         pugi::xml_node image = images.append_child("image");
         image.append_attribute("file") = (prepend_folder + "/" + path(image_location[i]).filename().string()).c_str();
-        for(auto const &rect : rects[i]){
-            pugi::xml_node box = image.append_child("box");
-            box.append_attribute("top") = rect.top();
-            box.append_attribute("left") = rect.left();
-            box.append_attribute("width") = rect.width();
-            box.append_attribute("height") = rect.height();
-        }
+        add_image_info(image, rects[i]);
     }
 
     doc.print(stream);
 }
 
-void write_dlib_xml(const std::vector<std::string> &image_location, const std::vector<std::vector<dlib::mmod_rect>> &rects,
-                    const std::string &prepend_folder, const std::string &output_name)
-{
-    std::vector<std::vector<dlib::rectangle>> simple_rects;
-    simple_rects.reserve(rects.size());
-    for(auto const &rect : rects){
-        std::vector<dlib::rectangle> temp;
-        for(auto const &r : rect){
-            temp.emplace_back(r.rect);
-        }
-        simple_rects.emplace_back(std::move(temp));
-    }
+}
 
-    write_dlib_xml(image_location, simple_rects, prepend_folder, output_name);
+void write_imglab_xml(const std::vector<std::string> &image_location, const std::vector<std::vector<dlib::rectangle>> &rects,
+                      const std::string &prepend_folder, const std::string &output_name)
+{
+    write_imglab_xml_impl(image_location, rects, prepend_folder, output_name);
+}
+
+void write_imglab_xml(const std::vector<std::string> &image_location, const std::vector<std::vector<dlib::mmod_rect>> &rects,
+                      const std::string &prepend_folder, const std::string &output_name)
+{
+    write_imglab_xml_impl(image_location, rects, prepend_folder, output_name);
 }
 
 }
